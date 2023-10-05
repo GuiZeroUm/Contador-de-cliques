d35ce7dfd81f56eecdc60de46688944d70afa8f8
Guilherme Santos <110872956+GuiZeroUm@users.noreply.github.com>
2023-10-04 20:28:04 -0500

    Criando botão

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import tkinter as tk
+
+root = tk.Tk()
+
+root.geometry('600x600') #mudando o tamanho da janela
+root.title ('Contador de clicks') #mudando o titulo do app
+root.configure (background='#1d1d1d') #adicionando cor de fundo
+
+margem = tk.Canvas(root, height=300, background='#1d1d1d', highlightthickness=0,  relief='flat')
+margem.pack() #fazendo com que a margem do botão apareça
+botao__foda = tk.Button(root, bg='#FFFFFF', text='Bota vai',) #criando o botão
+botao__foda.pack() #fazendo com que o botão apareça
+
+root.mainloop() #fazendo com que nossa janela não feche
